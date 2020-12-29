b174e37a7928bb7ca65962e0997be53d8344f519
Parvez <parvezmushtaak@gmail.com>
2020-12-29 21:04:40 +0100

    added some extra code

--- strings.py
+++ strings.py
@@ -27,5 +27,22 @@ print("     This string has no blanks!      ".strip())
 # string is just as a immutable list 
 print("print 'Only'"[-6:])
 
-#string substitution 
+# the code below is taken from https://docs.python.org/3/library/string.html
+#string substitution and text formatting 
 print("The names of friends are {0},{2},{1}".format(name2, name3, name4))
+point= (3,2)
+print("x_cord:{0[0]}, y_cord: {0[1]}".format(point)) ## accessing the arguments
+
+print("{:<30}".format("left aligned"))
+print("{:>30}".format("right aligned"))
+print("{:^30}".format("centered"))
+print("{:-^30}".format("centered"))
+
+print("{:,}".format(1234567890))    ## using comma as a thousands seperator
+
+print("Percentage(correct upto 4th decimal): {:.4%}".format(4567890/7684234))
+
+#type formatting
+import datetime
+d= datetime.datetime(2020, 12, 27, 18, 30, 55)
+print("the date and time is {: %d.%m.%Y %H:%M:%S}".format(d))
